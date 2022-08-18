077eaf83882f0de6c32272acd18ed2ce0704f771
Julia Branke <Julia.Branke@guardian.co.uk>
2022-08-18 12:30:31 +0100

    Applied prettier.

--- cdk/lib/backend-101-doctest-julia.ts
+++ cdk/lib/backend-101-doctest-julia.ts
@@ -1,11 +1,11 @@
-import type {GuStackProps} from '@guardian/cdk/lib/constructs/core';
-import {GuStack} from '@guardian/cdk/lib/constructs/core';
-import type {App} from 'aws-cdk-lib';
-import {GuEc2App} from "@guardian/cdk";
-import {AccessScope} from "@guardian/cdk/lib/constants";
-import {InstanceClass, InstanceSize, InstanceType} from "aws-cdk-lib/aws-ec2";
-import {GuCname} from "@guardian/cdk/lib/constructs/dns";
-import {Duration} from "aws-cdk-lib";
+import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
+import { GuStack } from '@guardian/cdk/lib/constructs/core';
+import type { App } from 'aws-cdk-lib';
+import { GuEc2App } from '@guardian/cdk';
+import { AccessScope } from '@guardian/cdk/lib/constants';
+import { InstanceClass, InstanceSize, InstanceType } from 'aws-cdk-lib/aws-ec2';
+import { GuCname } from '@guardian/cdk/lib/constructs/dns';
+import { Duration } from 'aws-cdk-lib';
 
 export class Backend101DoctestJulia extends GuStack {
 	constructor(scope: App, id: string, props: GuStackProps) {
@@ -13,7 +13,7 @@ export class Backend101DoctestJulia extends GuStack {
 
 		const name = 'backend-101-doctest-julia';
 		const domainName = `${name}.gutools.co.uk`;
-		const bucket = "deploy-tools-dist";
+		const bucket = 'deploy-tools-dist';
 
 		const keyPrefix = `${this.stack}/${this.stage}/${name}`;
 
@@ -22,22 +22,22 @@ export class Backend101DoctestJulia extends GuStack {
 		aws s3 cü s3://${bucket}/${keyPrefix}/hello-world.jar /hello-world.jar
 		systemctl start ${name}`;
 
-		const app=new GuEc2App(this,{
+		const app = new GuEc2App(this, {
 			applicationPort: 9000,
 			app: name,
-			access: {scope: AccessScope.PUBLIC},
+			access: { scope: AccessScope.PUBLIC },
 			instanceType: InstanceType.of(InstanceClass.T4G, InstanceSize.SMALL),
-			monitoringConfiguration: {noMonitoring: true},
-			scaling: {minimumInstances: 1, maximumInstances: 2},
+			monitoringConfiguration: { noMonitoring: true },
+			scaling: { minimumInstances: 1, maximumInstances: 2 },
 			userData: userData,
-			certificateProps: {domainName},
+			certificateProps: { domainName },
 		});
 
 		new GuCname(this, 'dns', {
 			app: name,
 			domainName,
 			ttl: Duration.minutes(1),
-			resourceRecord: app.loadBalancer.loadBalancerDnsName
+			resourceRecord: app.loadBalancer.loadBalancerDnsName,
 		});
 	}
 }
