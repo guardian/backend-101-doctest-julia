1ce2662b7e81693ee3c2d69bff0cf725388a88c9
Julia Branke <Julia.Branke@guardian.co.uk>
2022-08-18 13:11:45 +0100

    not sure why I needed to apply eslint manually

--- cdk/lib/backend-101-doctest-julia.ts
+++ cdk/lib/backend-101-doctest-julia.ts
@@ -1,11 +1,11 @@
-import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
-import { GuStack } from '@guardian/cdk/lib/constructs/core';
-import type { App } from 'aws-cdk-lib';
 import { GuEc2App } from '@guardian/cdk';
 import { AccessScope } from '@guardian/cdk/lib/constants';
-import { InstanceClass, InstanceSize, InstanceType } from 'aws-cdk-lib/aws-ec2';
+import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
+import { GuStack } from '@guardian/cdk/lib/constructs/core';
 import { GuCname } from '@guardian/cdk/lib/constructs/dns';
+import type { App } from 'aws-cdk-lib';
 import { Duration } from 'aws-cdk-lib';
+import { InstanceClass, InstanceSize, InstanceType } from 'aws-cdk-lib/aws-ec2';
 
 export class Backend101DoctestJulia extends GuStack {
 	constructor(scope: App, id: string, props: GuStackProps) {
